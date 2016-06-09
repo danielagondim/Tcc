6fceec4af066d79a8a8dc60d1865ad4ed4f363cd
danielagondim <daniela.gondim@hotmail.com>
2016-06-09 11:59:59 -0300

    Classe Usuario

---
+++ TCC.CursosOnline.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCC.CursosOnline.Dominio.Entidades
+{
+    class Usuario
+    {
+        public int Id_usuario { get; set; }
+        public bool Ativo { get; set; }
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Senha { get; set; }
+        public bool Administrador { get; set; }
+    }
+}
