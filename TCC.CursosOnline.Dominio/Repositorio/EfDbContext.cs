568bf8556f7c5cbf8760d85f5fc1e0baee1f89d6
danielagondim <daniela.gondim@hotmail.com>
2016-06-24 12:01:44 -0300

    Alteracoes

--- TCC.CursosOnline.Dominio/Repositorio/EfDbContext.cs
+++ TCC.CursosOnline.Dominio/Repositorio/EfDbContext.cs
@@ -6,12 +6,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TCC.CursosOnline.Dominio.Entidades;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace TCC.CursosOnline.Dominio.Repositorio
 {
     public class EfDbContext : DbContext
     {
+        public DbSet<Andamento> Andamentos { get; set; }
+        public DbSet<Atividade> Atividades { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Forum_resposta> Forum_respostas { get; set; }
+        public DbSet<Forum_topico> Forum_topicos { get; set; }
+        public DbSet<Inscricao> Inscricoes { get; set; }
+        public DbSet<Material> Materiais { get; set; }
+        public DbSet<Opcao> Opcoes { get; set; }
+        public DbSet<Questao> Questoes { get; set; }
+        public DbSet<Resposta> Respostas { get; set; }
+        public DbSet<Resultado> Resultados { get; set; }
+        public DbSet<Unidade> Unidades { get; set; }        
         public DbSet<Usuario> Usuarios { get; set;  }
+        public DbSet<Video> Videos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Andamento>().ToTable("Andamentos");
+            modelBuilder.Entity<Atividade>().ToTable("Atividades");
+            modelBuilder.Entity<Categoria>().ToTable("Categorias");
+            modelBuilder.Entity<Curso>().ToTable("Cursos");
+            modelBuilder.Entity<Forum_resposta>().ToTable("Forum_Respostas");
+            modelBuilder.Entity<Forum_topico>().ToTable("Forum_Topicos");
+            modelBuilder.Entity<Inscricao>().ToTable("Incricoes");
+            modelBuilder.Entity<Material>().ToTable("Materiais");
+            modelBuilder.Entity<Opcao>().ToTable("Opcoes");
+            modelBuilder.Entity<Questao>().ToTable("Questoes");
+            modelBuilder.Entity<Resposta>().ToTable("Respostas");
+            modelBuilder.Entity<Resultado>().ToTable("Resultados");
+            modelBuilder.Entity<Unidade>().ToTable("Unidades");
+            modelBuilder.Entity<Usuario>().ToTable("Usuarios");
+            modelBuilder.Entity<Video>().ToTable("Videos");
+
+        }
 
     }
 }
---
+++ TCC.CursosOnline.Dominio/Repositorio/UsuariosRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCC.CursosOnline.Dominio.Entidades;
+
+namespace TCC.CursosOnline.Dominio.Repositorio
+{
+    public class UsuariosRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Usuario> Usuarios
+        {
+            get { return _context.Usuarios; }
+        }
+
+    }
+}
