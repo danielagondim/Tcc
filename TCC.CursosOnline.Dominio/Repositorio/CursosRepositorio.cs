430bfb090f3befa237359a81afc3c92b89759ea4
danielagondim <daniela.gondim@hotmail.com>
2016-06-27 22:07:01 -0300

    Inicio Cursos

---
+++ TCC.CursosOnline.Dominio/Repositorio/CursosRepositorio.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCC.CursosOnline.Dominio.Entidades;
+
+namespace TCC.CursosOnline.Dominio.Repositorio
+{
+    public class CursosRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public List<Curso> ListaTodosCursos()
+        {
+            return _context.Cursos.ToList();
+        }
+
+        public Curso ListaCursoPorId(Int32 Id)
+        {
+
+            return _context.Cursos.FirstOrDefault(p => p.Id_curso == Id);
+        }
+
+        //Salvar ou Alterar um Curso
+        public void Salvar(Curso Curso)
+        {
+            if (Curso.Id_curso == 0)
+            {
+                //Salvar
+                _context.Cursos.Add(Curso);
+
+
+            }
+            else
+            {
+                Curso CursoBanco = _context.Cursos.Find(Curso.Id_curso);
+                if (CursoBanco != null)
+                {
+                    //Alterar
+                    CursoBanco.Ativo = Curso.Ativo;
+                    CursoBanco.Id_categoria = Curso.Id_categoria;
+                    CursoBanco.Titulo_curso = Curso.Titulo_curso;
+
+                }
+            }
+
+            _context.SaveChanges();
+
+        }
+
+        //Desativar
+        public Curso Desativar(int Id)
+        {
+            Curso CursoBanco = _context.Cursos.Find(Id);
+
+            if (CursoBanco != null)
+            {
+                CursoBanco.Ativo = false;
+                _context.SaveChanges();
+
+            }
+
+            return CursoBanco;
+        }
+    }
+}
---
+++ TCC.CursosOnline.Web/Areas/Admin/Controllers/CursosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TCC.CursosOnline.Dominio.Repositorio;
+using TCC.CursosOnline.Dominio.Entidades;
+
+namespace TCC.CursosOnline.Web.Areas.Admin.Controllers
+{
+    public class CursosController : Controller
+    {
+        private CursosRepositorio _repositorio;
+
+        // GET: Admin/Cursos
+        public ActionResult Index()
+        {
+            _repositorio = new CursosRepositorio();
+            var Cursos = _repositorio.ListaTodosCursos();
+            return View(Cursos);
+        }
+    }
+}
